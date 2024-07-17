a5e04c6ddfd24bba29645edfc6660e86f57cdc29
Indigenous1151 <89427482+Indigenous1151@users.noreply.github.com>
2024-07-17 10:33:42 -0500

    Update script.js
    updated functionality for registration and login buttons

--- script.js
+++ script.js
@@ -1,36 +1,59 @@
 const wrapper = document.querySelector('.wrapper');
 const loginBtn = document.querySelector('.loginBtn-popup');
 const closeBtn = document.querySelector('.close-icon');
-const submitBtn = document.querySelector('.btn');
 const eyeIcon = document.querySelector('.toggle-visible');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const notifyIncorrect = document.querySelector('.notify-incorrect');
 
 loginBtn.addEventListener('click', ()=>{
-    wrapper.classList.add('active-popup');
+    if (wrapper.classList.contains('active-popup')) {
+        document.getElementById("userEmail").value="";
+        document.getElementById("userPassword").value="";
+        document.getElementById("email").value="";
+        document.getElementById("password").value="";
+        wrapper.classList.remove('active-popup');
+
+        if (wrapper.classList.contains('active')) {
+            wrapper.classList.remove('active');
+        }
+    }
+    else {
+        wrapper.classList.add('active-popup');
+    }
 });
 
 closeBtn.addEventListener('click', ()=>{
     wrapper.classList.remove('active-popup');
+
+    document.getElementById("userEmail").value="";
+    document.getElementById("userPassword").value="";
+    document.getElementById("email").value="";
+    document.getElementById("password").value="";
 });
 
-// take in the text from email and password inputs
-// hash inputted values
-// compared hashed values with database for valid credentials
-function getUserInput() {
-    let userEmail = document.getElementById("userEmail").value;
-    let userPassword = document.getElementById("userPassword").value;
-    alert(userEmail + ' ' + userPassword);
-}
+registerLink.addEventListener('click', ()=>{
+    wrapper.classList.add('active');
+    document.getElementById("userEmail").value="";
+    document.getElementById("userPassword").value="";
+});
+
+loginLink.addEventListener('click', ()=>{
+    wrapper.classList.remove('active');
+    document.getElementById("email").value="";
+    document.getElementById("password").value="";
+});
 
 
 function toggleVisible() {
     let userPassword = document.getElementById("userPassword");
 
     if (userPassword.type === "password") {
-        userPassword.type = "text";
-        eyeIcon.tagName = "eye-off";
+        userPassword.type="text";
+        eyeIcon.tagName="eye-off";
     }
     else {
-        userPassword.type = "password";
-        eyeIcon.tagName = "eye";
+        userPassword.type="password";
+        eyeIcon.tagName="eye";
     }
 }
